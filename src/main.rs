1098016fc504d96a95adcd6e40244e0f2e88f428
Carter Watson <54688033+cartwatson@users.noreply.github.com>
2025-07-27 19:50:01 -0700

    fix: formatting; fix: layout + debt;

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use std::fs;
-use std::fs::File;
-use std::io::prelude::*;
+use std::io::Write;
 use std::process::Command;
 
 use regex::Regex;
@@ -15,90 +14,64 @@ fn get_first_char(line: &str) -> char {
 }
 
 fn main() {
-    // hardcoded variables... 0_0
-    let read_file_path = "content/test.md"; // DEBUG: delete once content_dir works
-    // let content_dir: String  = "content/".to_string();
+    // TODO: V1: hardcoded variables... 0_0
+    let content_dir: String = "content/".to_string();
     let template_dir: String = "template/".to_string();
     let template_file: String = "template.html".to_string();
     let template_splitter: String = "<div id=\"content\">".to_string();
     let output_dir: String = "out/".to_string();
 
-    // TODO: copy over all non-html files from ./template to ./out
-    // V0 copy all files and rm template.html
-    let _ = Command::new("cp")
-        .arg("-r")
-        .arg(&template_dir)
-        .arg(&output_dir)
-        .output();
-    let _ = Command::new("rm")
-        .arg(output_dir.clone() + &template_file)
-        .output();
+    // TODO: V1: copy over all non-html files from ./template to ./out
+    let _ = Command::new("cp").arg("-r").arg(&template_dir).arg(&output_dir).output();
+    let _ = Command::new("rm").arg(output_dir.clone() + &template_file).output();
 
     // grab template file
-    let template_file = fs::read_to_string(template_dir + &template_file)
-        .expect("Should have been able to read template file");
+    let template_file = fs::read_to_string(template_dir + &template_file).expect("Should have been able to read template file");
     // break up template write it back to output file
     let template_file_split: Vec<_> = template_file.split(&template_splitter).collect();
     let header: String = template_file_split[0].to_string() + &template_splitter; // TODO: figure out way to not have to add split back to file
     let footer: String = template_file_split[1].to_string();
 
-    // read file in
-    // TODO: recursively read all files in from content_dir
-    let contents =
-        fs::read_to_string(read_file_path).expect("Should have been able to read the file");
+    // recursively read all files in from content_dir
+    for file_name in get_files_recursive(content_dir.clone()) {
+        let read_file_path = content_dir.clone() + &file_name;
+        let write_file_path: String = output_dir.clone() + &remove_extension(read_file_path.to_string()) + ".html";
 
-    // WARN: -------------------------------------------------------------------
-    // NOTE: everything below should be done recursively for each content file
-    // WARN: -------------------------------------------------------------------
+        // grab .md
+        let contents = fs::read_to_string(read_file_path.clone()).expect("Should have been able to read the file");
 
-    // write file out
-    let write_file_path: String = output_dir + &get_file_name(read_file_path.to_string()) + ".html";
-    let mut output_file =
-        File::create(write_file_path).expect("Should have been able to create {write_file_path}");
+        // init out file
+        let mut output_buffer: fs::File = fs::File::create(write_file_path).expect("Should have been able to create {write_file_path}");
+        let _ = output_buffer.write(&header.clone().into_bytes());
 
-    let _ = output_file.write(&header.into_bytes());
-
-    //------------------------------------------------------------------------------
-    // TODO: FIX THIS SHIT
-    // TODO: create stack to hold current state
-    // let mut state_stack: Vec<String> = Vec::new();
-    let mut content: String = String::new();
-    for line in contents.lines() {
-        // get to front of line and determine what type of line it is
-        let first_char = get_first_char(line);
-
-        match first_char {
-            '#' => content += &heading_processer(line),
-            // '>' =>    output_file.write_all(b"Blockquote\n").expect("error writing to file"),
-            // '-'|'*' => output_file.write_all(b"Bulleted List\n").expect("error writing to file"),
-            // '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => output_file.write_all(b"Ordered List\n").expect("error writing to file"),
-            // '`' => output_file.write_all(b"Codeblock\n").expect("error writing to file"),
-            // blank line, empty state_stack completely
-            // '\n' => continue,
-            // assume the line is regular HTML
-            // '<' => content += line,
-            // paragraph
-            _ => content += &paragraph_processer(line),
+        //------------------------------------------------------------------------------
+        // TODO: V1: FIX THIS SHIT; create stack to hold current state
+        let mut html_content: String = String::new();
+        for line in contents.lines() {
+            // get to front of line and determine what type of line it is
+            let first_char = get_first_char(line);
+            match first_char {
+                '#' => html_content += &heading_processer(line),
+                // '-'|'*' => output_file.write_all(b"Bulleted List\n").expect("error writing to file"),
+                // '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => output_file.write_all(b"Ordered List\n").expect("error writing to file"),
+                // '`' => output_file.write_all(b"Codeblock\n").expect("error writing to file"),
+                // '>' => output_file.write_all(b"Blockquote\n").expect("error writing to file"),
+                // blank line, empty state_stack completely
+                _ => html_content += &paragraph_processer(line),
+                // '\n' => continue,
+                // '<' => content += line; // assume the line is regular HTML
+            }
         }
-    }
-    //------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------
 
-    let _ = output_file.write(&content.into_bytes());
-    let _ = output_file.write(&footer.into_bytes());
+        let _ = output_buffer.write(&html_content.into_bytes());
+        let _ = output_buffer.write(&footer.clone().into_bytes());
+    }
 }
 
-fn get_file_name(full_name: String) -> String {
-    // remove everything before the file name (everything after last '/')
-    let mut temp = full_name
-        .split("/")
-        .collect::<Vec<_>>()
-        .last()
-        .expect("COULDNT SPLIT")
-        .to_string();
+fn remove_extension(full_name: String) -> String {
     // remove file extentension (everything before '.')
-    temp = temp.split(".").collect::<Vec<_>>()[0].to_string();
-
-    return temp;
+    return full_name.split(".").collect::<Vec<_>>()[0].to_string(); // HACK: V1: fix
 }
 
 fn heading_processer(line: &str) -> String {
@@ -118,10 +91,41 @@ fn paragraph_processer(line: &str) -> String {
     }
 
     // use regex to filter links
+    //
     // [text](link) // no white space in link FYI
     let links_regex = Regex::new(r"\[(?<text>.*?)\]\((?<link>\S+?)\)").unwrap();
-    let formatted_line = links_regex.replace_all(line, "<a href=\"${link}\">${text}</a> ");
+    let formatted_line = links_regex.replace_all(line, "<a href=\"${link}\">${text}</a> "); // TODO: V1: add target="_"; internal links redirecter, external pages send to new tab
+
+    // TODO: V1:
+    // images
+    // bold
+    // italic
+    // inline code
 
     let html = format!("<p>{formatted_line}</p>\n");
     return html;
+}
+
+fn get_files_recursive(base_dir: String) -> Vec<String> {
+    // TODO: V0: recursively read all files in from content_dir
+    // input "content/"
+    //
+    // content/
+    // ├── blog
+    // │   ├── article1.md
+    // │   └── article2.md
+    // └── test.md
+    //
+    // returns [
+    //   blog/article1.md
+    //   blog/article2.md
+    //   test.md
+    // ]
+    //
+    // NOTE: do NOT return "content/" in file name
+
+    let entries: Vec<String> = vec![];
+    // let temp = fs::read_dir(base_dir);
+    // // entries.sort();
+    return entries;
 }
